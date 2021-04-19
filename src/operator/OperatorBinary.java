a827e3be19ed6ee8e0b6275d567fed58913abbe0
wahyud1n <wahyud768@gmail.com>
2021-04-19 12:28:00 +0800

    Operator Binary Assigment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Wahyudin";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.90;
+
+}

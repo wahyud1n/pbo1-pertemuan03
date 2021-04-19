f5102d011a70eccebb76bfb16a7eb82bf7d2bbf0
wahyud1n <wahyud768@gmail.com>
2021-04-19 12:35:57 +0800

    Operator Unary

---
+++ src/operator/OperatorUnary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorUnary {
+    public static void main(String[] args) {
+        int i = 0;
+        i++;                        //i tadinya 0 ditambahkan 1 sehingga i=1
+        System.out.println(i);      //betulkan i nilainya 1
+        i++;                        //karena i tadi sudah bernilai 1, maka sekarang ditambahkan lagi 1 menjadi 2
+        System.out.println(i);      //tuh betulkan i nilainya 2
+        i=i+1;                      //bisa dilihat i++ sama dengan i= i+1
+        System.out.println(i);      // i tadi 2 sekarang menjadi 3
+
+    }
+}

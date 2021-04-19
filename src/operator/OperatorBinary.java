dbe3a0c414fc50060da42ba1274349e01a6dfda3
wahyud1n <wahyud768@gmail.com>
2021-04-19 12:30:35 +0800

    Operator Binary Aritmarika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -9,4 +9,13 @@ public class OperatorBinary {
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.90;
 
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+
+    }
 }
